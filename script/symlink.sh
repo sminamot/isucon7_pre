ff82e381d3a3995cb249ccdaaaae6cb2b0c21230
sminamot <shosfs@gmail.com>
2018-09-04 21:05:12 +0900

    update

--- script/symlink.sh
+++ script/symlink.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo ln -sf /home/isucon/isucon7-pre/config/nginx.conf /etc/nginx/nginx.conf
-sudo ln -sf /home/isucon/isucon7-pre/config/my.cnf /etc/mysql/my.cnf
-sudo ln -sf /home/isucon/isucon7-pre/go/src/isubata/isubata /home/isucon/isubata/webapp/go/isubata
+sudo ln -sf /home/isucon/isucon7_pre/config/nginx.conf /etc/nginx/nginx.conf
+sudo ln -sf /home/isucon/isucon7_pre/config/my.cnf /etc/mysql/my.cnf
+sudo ln -sf /home/isucon/isucon7_pre/go/src/isubata/isubata /home/isucon/isubata/webapp/go/isubata
